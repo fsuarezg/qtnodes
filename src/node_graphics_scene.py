9e56282d3f13a2c5d56dc169d860955796b78e26
fernandosg <fernando_suarez_groen@outlook.com>
2024-08-19 22:26:42 +0200

    Add node_graphics_scene
    Added a GraphicsScene class that can draw a grid.

---
+++ src/node_graphics_scene.py
@@ -0,0 +1,76 @@
+from PySide6 import QtWidgets
+from PySide6 import QtGui
+from PySide6 import QtCore 
+import math
+
+class GraphicsScene(QtWidgets.QGraphicsScene):
+    def __init__(self, parent=None):
+        super(GraphicsScene, self).__init__(parent)
+        self.initialize_ui()
+        
+    @property
+    def _color_background(self):
+        return QtGui.QColor('#393939')
+    
+    @property
+    def _color_light(self):
+        return QtGui.QColor('#2f2f2f')
+    
+    @property
+    def _color_dark(self):
+        return QtGui.QColor('#292929')
+    
+    @property
+    def _grid_size(self):
+        return 20
+    
+    @property
+    def _grid_squares(self):
+        return 5
+    
+    def initialize_ui(self):
+        self._pen_light = QtGui.QPen(self._color_light)
+        self._pen_light.setWidth(1)
+        self._pen_dark = QtGui.QPen(self._color_dark)
+        self._pen_dark.setWidth(2)
+        self.setBackgroundBrush(self._color_background)
+        self.scene_width, self.scene_height = 64000, 64000
+        self.setSceneRect(-self.scene_width//2,
+                          -self.scene_height//2, 
+                          self.scene_width,
+                          self.scene_height)
+        
+    def drawBackground(self, painter, rect):
+        super().drawBackground(painter, rect)
+
+        # create grid
+        left = int(math.floor(rect.left()))
+        right = int(math.ceil(rect.right()))
+        top = int(math.floor(rect.top()))
+        bottom = int(math.ceil(rect.bottom()))
+
+        first_left = left - (left % self._grid_size)
+        first_top = top - (top % self._grid_size)
+        
+        # compute all lines to be drawn
+        lines_light, lines_dark = [], []
+        for x in range(first_left, right, self._grid_size):
+            if (x % (self._grid_size*self._grid_squares) != 0): 
+                lines_light.append(QtCore.QLine(x, top, x, bottom))
+            else: 
+                lines_dark.append(QtCore.QLine(x, top, x, bottom))
+
+        for y in range(first_top, bottom, self._grid_size):
+            if (y % (self._grid_size*self._grid_squares) != 0): 
+                lines_light.append(QtCore.QLine(left, y, right, y))
+            else: 
+                lines_dark.append(QtCore.QLine(left, y, right, y))
+
+        # draw the lines
+        painter.setPen(self._pen_light)
+        painter.drawLines(lines_light)
+
+        painter.setPen(self._pen_dark)
+        painter.drawLines(lines_dark)
+
+    
