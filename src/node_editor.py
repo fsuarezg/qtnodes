c285d111ac8aa933b6be9481a6368f298c46949c
fernandosg <fernando_suarez_groen@outlook.com>
2024-08-19 22:25:43 +0200

    Set window title node_editor
    Setting window title of the node editor to be "Node Editor"
    Rename 'initialize()' to 'initialize_ui()'

--- src/node_editor.py
+++ src/node_editor.py
@@ -1,12 +1,23 @@
 from PySide6 import QtWidgets
 
+from node_graphics_scene import GraphicsScene
+from node_graphics_view import GraphicsView
+
 class NodeEditor(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(NodeEditor, self).__init__(parent)
-        self.initialize()
+        self.initialize_ui()
 
-    def initialize(self):
+    def initialize_ui(self):
         self.setGeometry(200, 200, 800, 600)
 
-        self.setWindowTitle("HELLO")
+        self.layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.grScene = GraphicsScene()
+
+        self.view = GraphicsView(self.grScene, self)
+        self.layout.addWidget(self.view)
+
+        self.setWindowTitle("Node Editor")
         self.show()
