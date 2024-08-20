9df312aa116578b4839d46e04093e5c5a6070a2a
fernandosg <fernando_suarez_groen@outlook.com>
2024-08-20 20:11:45 +0200

    Add drag and zoom to graphics view
    Add the ability to zoom in/out by scrolling and dragging by clicking the middle mouse button. The zoom also follows the mouse cursor.

--- src/node_graphics_view.py
+++ src/node_graphics_view.py
@@ -8,6 +8,13 @@ class GraphicsView(QtWidgets.QGraphicsView):
         self.grScene = grScene
         self.initialize_ui()
         self.setScene(self.grScene)
+
+        self.zoomInFactor = 1.25
+        self.zoomOutFactor = 1 / self.zoomInFactor
+        self.zoomClamp = False
+        self.zoom = 10
+        self.zoomStep = 1
+        self.zoomRange = [0, 10]
     
     def initialize_ui(self):
         # Make any drawn items smoother with antialiasing
@@ -19,4 +26,86 @@ class GraphicsView(QtWidgets.QGraphicsView):
         # Turn off scroll bars
         # self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         # self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+        # Apply transformation under mouse position
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        elif event.button() == QtCore.Qt.LeftButton:
+            self.rightMouseButtonPress(event)
+        elif event.button() == QtCore.Qt.RightButton:
+            self.rightMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        elif event.button() == QtCore.Qt.LeftButton:
+            self.leftMouseButtonRelease(event)
+        elif event.button() == QtCore.Qt.RightButton:
+            self.rightMouseButtonRelease(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+
+    def middleMouseButtonPress(self, event):
+        """Allow dragging view upon middle mouse press
+
+        Args:
+            event (_type_): _description_
+        """        
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        fakeEvent = QtGui.QMouseEvent(
+            event.type(), event.localPos(), event.screenPos(),
+            QtCore.Qt.LeftButton, event.buttons() | QtCore.Qt.LeftButton,
+            event.modifiers())
+        super().mousePressEvent(fakeEvent)
+
+
+
+    def middleMouseButtonRelease(self, event):
+        self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+
+
+    def leftMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def leftMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+
+    def rightMouseButtonPress(self, event):
+        return super().mousePressEvent(event)
+
+    def rightMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event)
+    
+    def wheelEvent(self, event):
+        # Store scene position
+        # BUG: Pyside 6.7 has a bug where mapToScene needs to have exact 
+        # (int, int) type passed. Normally I should be able to pass on the
+        # event.position() directly
+        pos = event.position()
+        oldPos = self.mapToScene(pos.x(), pos.y())
+
+        # Calculate zoom
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor
+            self.zoom += self.zoomStep
+        else:
+            zoomFactor = self.zoomOutFactor
+            self.zoom -= self.zoomStep
+
+        # Set scene scale 
+        self.scale(zoomFactor, zoomFactor)
+
+        # Translate view
+        # BUG: see above
+        pos = event.position()
+        newPos = self.mapToScene(pos.x(), pos.y())
+        delta = newPos - oldPos
+        self.translate(delta.x, delta.y)
+
 
