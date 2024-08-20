598817c03023a8a73bd0295ee4134c1ed669b46f
fernandosg <fernando_suarez_groen@outlook.com>
2024-08-20 22:44:48 +0200

    Use SceneManager in NodeEditor
    Instead of the GraphicsScene I now initialize a SceneManager which contains the GraphicsScene in the NodeEditor

--- src/node_editor.py
+++ src/node_editor.py
@@ -1,23 +1,31 @@
 from PySide6 import QtWidgets
+from PySide6 import QtGui
 
-from node_graphics_scene import GraphicsScene
+from scene_manager import SceneManager
 from node_graphics_view import GraphicsView
 
 class NodeEditor(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(NodeEditor, self).__init__(parent)
-        self.initialize_ui()
+        self.initUI()
 
-    def initialize_ui(self):
+    def initUI(self):
         self.setGeometry(200, 200, 800, 600)
 
         self.layout = QtWidgets.QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.grScene = GraphicsScene()
+        self.scene = SceneManager()
+        self.grScene = self.scene.grScene
 
         self.view = GraphicsView(self.grScene, self)
         self.layout.addWidget(self.view)
 
         self.setWindowTitle("Node Editor")
         self.show()
+
+        greenBrush = QtGui.QBrush(QtGui.Qt.green)
+        outlinePen = QtGui.QPen(QtGui.Qt.black)
+        outlinePen.setWidth(2)
+        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        rect.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
