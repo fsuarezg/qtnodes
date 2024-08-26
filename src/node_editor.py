2bc07ffca431d426aa37ca70c341020d0cc91bc4
fernandosg <fernando_suarez_groen@outlook.com>
2024-08-26 22:08:43 +0200

    Edit node_editor to create to contain a simple node
    Instead of the green test rectangle, I now create a simple node in the node editor.

--- src/node_editor.py
+++ src/node_editor.py
@@ -2,7 +2,8 @@ from PySide6 import QtWidgets
 from PySide6 import QtGui
 
 from scene_manager import SceneManager
-from src.graphics_view import GraphicsView
+from graphics_view import GraphicsView
+from node.node import Node
 
 class NodeEditor(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -16,16 +17,18 @@ class NodeEditor(QtWidgets.QWidget):
         self.setLayout(self.layout)
 
         self.scene = SceneManager()
-        self.grScene = self.scene.grScene
+        #self.grScene = self.scene.grScene
 
-        self.view = GraphicsView(self.grScene, self)
+        node = Node(self.scene, "My Awesome Node")
+
+        self.view = GraphicsView(self.scene.grScene, self)
         self.layout.addWidget(self.view)
 
         self.setWindowTitle("Node Editor")
         self.show()
 
-        greenBrush = QtGui.QBrush(QtGui.Qt.green)
-        outlinePen = QtGui.QPen(QtGui.Qt.black)
-        outlinePen.setWidth(2)
-        rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
-        rect.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
+        # greenBrush = QtGui.QBrush(QtGui.Qt.green)
+        # outlinePen = QtGui.QPen(QtGui.Qt.black)
+        # outlinePen.setWidth(2)
+        # rect = self.grScene.addRect(-100, -100, 80, 100, outlinePen, greenBrush)
+        # rect.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
