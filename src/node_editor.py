fcf368dfe300d0b1a1f137f31a3196930ea7a977
Fernando Suarez Groen <fernandos@nwavedigital.com>
2024-08-28 15:11:21 +0200

    Add addDebugNodes in NodeEditor
    Add a function to create a debug setup of nodes and edges.

--- src/node_editor.py
+++ src/node_editor.py
@@ -4,6 +4,8 @@ from PySide6 import QtWidgets
 from scene_manager import SceneManager
 from graphics_view import GraphicsView
 from node.node import Node
+from node.edge import Edge
+
 
 
 class NodeEditor(QtWidgets.QWidget):
@@ -20,14 +22,30 @@ class NodeEditor(QtWidgets.QWidget):
         self.scene = SceneManager()
         # self.grScene = self.scene.grScene
 
-        node = Node(self.scene, "My Awesome Node", 
-                    inputs=[1, 2, 3], outputs=[1])
+        self.addDebugNodes()
+        # node = Node(self.scene, "My Awesome Node", 
+        #             inputs=[1, 2, 3], outputs=[1])
 
         self.view = GraphicsView(self.scene.grScene, self)
         self.layout.addWidget(self.view)
 
         self.setWindowTitle("Node Editor")
         self.show()
+
+    def addDebugNodes(self):
+        node1 = Node(self.scene, "My Awesome Node 1", 
+                     inputs=[1, 2, 3], outputs=[1])
+        node2 = Node(self.scene, "My Awesome Node 2",
+                     inputs=[1, 2, 3], outputs=[1])
+        node3 = Node(self.scene, "My Awesome Node 3",
+                     inputs=[1, 2, 3], outputs=[1])
+        node1.setPos(-350, -250)
+        node2.setPos(-75, 0)
+        node3.setPos(200, -150)
+
+        edge1 = Edge(self.scene, node1.outputs[0], node2.inputs[0])
+        edge2 = Edge(self.scene, node2.outputs[0], node3.inputs[0], type=2)
+
 
         # greenBrush = QtGui.QBrush(QtGui.Qt.green)
         # outlinePen = QtGui.QPen(QtGui.Qt.black)
