20679dab13f8f15e2a868de3c739b95ea99d3615
Fernando Suarez Groen <fernandos@nwavedigital.com>
2024-08-29 17:03:05 +0200

    Remove useless initUI call in main

--- src/main.py
+++ src/main.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     # app.setStyleSheet(str(file.readAll(), 'utf-8'))
 
     my_main_window = NodeEditor()
-    my_main_window.initUI()
 
     res = app.exec()
     sys.exit(res)
