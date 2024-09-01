26c0d3aed244073d8fb299445880d9f577b624ab
fernandosg <fernando_suarez_groen@outlook.com>
2024-09-01 13:05:45 +0200

    Improve drawing bezier curves to be smoother

--- src/node/graphics_edge.py
+++ src/node/graphics_edge.py
@@ -2,9 +2,13 @@ from PySide6 import QtWidgets
 from PySide6 import QtCore
 from PySide6 import QtGui
 from abc import abstractmethod
+import math
 
 from config.colors import (COLOR_EDGE, COLOR_EDGE_SELECTED)
+from node.socket import (RIGHT_TOP, RIGHT_BOTTOM,
+                         LEFT_TOP, LEFT_BOTTOM)
 
+EDGE_CP_ROUNDNESS = 100
 
 class GraphicsEdge(QtWidgets.QGraphicsPathItem):
     def __init__(self, edge, parent=None):
@@ -65,11 +69,27 @@ class GraphicsEdgeBezier(GraphicsEdge):
         s = self.posSource
         d = self.posDestination
         dist = (d[0] - s[0]) * 0.5
-        if s[0] > d[0]:
-            dist *= -1
+        cpx_s = +dist
+        cpy_s = 0
+        cpx_d = -dist
+        cpy_d = 0
+
+        sspos = self.edge.start_socket.position
+
+        if (s[0] > d[0] and sspos in (RIGHT_TOP, RIGHT_BOTTOM)
+            or (s[0] < d[0] and sspos in (LEFT_BOTTOM, LEFT_TOP))):
+            cpx_d *= -1
+            cpx_s *= -1
+
+            # more rounded bezier curves
+            div_value = (s[1] - d[1]) if (s[1] - d[1]) != 0 else 0.0000001
+            cpy_d = ((s[1] - d[1]) / math.fabs(div_value)) * EDGE_CP_ROUNDNESS
+            div_value = (d[1] - s[1]) if (d[1] - s[1]) != 0 else 0.0000001
+            cpy_s = ((d[1] - s[1]) / math.fabs(div_value)) * EDGE_CP_ROUNDNESS
 
         path = QtGui.QPainterPath(QtCore.QPointF(self.posSource[0],
                                                  self.posSource[1]))
-        path.cubicTo(s[0] + dist, s[1], d[0] - dist, d[1],
+        path.cubicTo(s[0] + cpx_s, s[1] + cpy_s,
+                     d[0] + cpx_d, d[1] + cpy_d,
                      self.posDestination[0], self.posDestination[1])
         self.setPath(path)
